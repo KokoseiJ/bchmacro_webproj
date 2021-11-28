4b2776bc8b97fb4ba079c59c1c02eeced27a1e2a
KokoseiJ <kokoseij@gmail.com>
2021-11-27 17:29:17 -0800

    Fixed CSS and list.html image size

--- app/views/index.py
+++ app/views/index.py
@@ -5,6 +5,7 @@ from flask import (
 from app.modules.auth import login_handler
 
 from os.path import join
+from urllib.parse import urlparse, urlunparse
 
 bp = Blueprint(
     name="index",
@@ -15,7 +16,7 @@ bp = Blueprint(
 
 @bp.route("/", methods=["GET"])
 def index():
-    # return render_template('index/index.html')
+    print(request.environ['wsgi.url_scheme'])
     return redirect("/questions")
 
 
@@ -24,3 +25,13 @@ def favicon():
     return send_from_directory(join(bp.root_path, 'app', 'static'),
                                'favicon.ico',
                                mimetype='image/vnd.microsoft.icon')
+
+
+@bp.get("/webfont/nanum-barun-gothic.css")
+def nanumbarun_webfont():
+    baseurl = urlunparse(urlparse(request.url)[:2] + ("",) * 4)
+    print(baseurl)
+    return render_template(
+        "resource/nanum-barun-gothic.css",
+        baseurl=baseurl
+    )
